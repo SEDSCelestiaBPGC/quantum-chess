0fdb1be425fc5c7c392a5b128add0da40c7940b7
ayushidubal <64092360+ayushidubal@users.noreply.github.com>
2020-07-22 18:15:29 +0530

    Changes to board (#7)
    * Update Board.py
    
    * Update Board.py
    
    Co-authored-by: ayushidubal <ayushidubal@users.noreply.github.com>

--- Code/Board.py
+++ Code/Board.py
@@ -2,12 +2,17 @@ import pygame
 
 light_square=(255,255,204)
 dark_square=(26,4,0)
+white=(255,255,255)
+black=(0,0,0)
+grey=(169,169,169)
 
 def make_board():
     pygame.init()
     board_width=480
+    margin=30
     board_height=board_width
-    board = pygame.display.set_mode((board_width, board_height))
+    board = pygame.display.set_mode((board_width+margin, board_height+margin))
+    board.fill(white)
     cols=[light_square, dark_square]
     sq_dim=board_width/8
 
@@ -15,6 +20,18 @@ def make_board():
         for j in range(0,8):
             sq_col=cols[(i+j)%2]
             pygame.draw.rect(board, sq_col, ((i*sq_dim), (j*sq_dim), sq_dim, sq_dim))
+  
+    label_font=pygame.font.SysFont("calibri", 18)
+
+    for i in range(0,8):
+        #pygame.draw.line(board, grey, (board_width, (i+1)*sq_dim), (board_width+margin, (i+1)*sq_dim), 2)
+        #pygame.draw.line(board, grey, ((i+1)*sq_dim, board_height), ((i+1)*sq_dim, board_height+margin), 2)    
+        col_text=label_font.render(chr(ord('A')+i), True, black)
+        row_text=label_font.render(str(8-i), True, black)
+        board.blit(col_text, ((i*sq_dim)+22, board_height+8))
+        board.blit(row_text, ((board_width+10), (i*sq_dim)+22))
+
+    pygame.draw.lines(board, black, True, [(0,0), (board_width, 0), (board_width, board_height), (0, board_height)], 2)
 
     pygame.display.flip()
 
